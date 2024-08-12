ebeee02c963fcac0ce5567ee5e4021daec88a099
reza.solghar@gmail.com <mras.arab@gmail.com>
2024-08-12 13:12:51 +0200

    batman

---
+++ src/test/java/com/pooya/payment/PayPalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pooya.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
